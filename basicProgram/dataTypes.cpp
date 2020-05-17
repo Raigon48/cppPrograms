09656ba49e6899a8cf928dc2722ee5726afc1c21
Abhishek_Rai <raigon48@gmail.com>
2020-05-17 15:03:42 +0530

    basic datatypes

---
+++ basicProgram/dataTypes.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main() 
+{
+    int num = 10;
+    float fnum = 10.6666;
+    double dnum = 10.66666666666;
+    char character = 'A';
+    string name = "Abhishek Rai";
+    bool isMale = true;
+
+    cout << "Number is represented using int : " << num << endl;
+    cout << "Floating point is represented using float : " << fnum << endl;
+    cout << "Double is represented using double : " << dnum << endl;
+    cout << "Character is represented using char : " << character << endl;
+    cout << "String is represented using string : " << name << endl;
+    cout << "Also keep in mind that strings must be wrapped between double quotes" << endl;
+    cout << "Boolen data is represented using bool : " << isMale << endl ;
+
+    return 0;
+}
