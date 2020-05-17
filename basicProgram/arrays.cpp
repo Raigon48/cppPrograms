d8e666e4a4a74b58e323f2b7927292276254d365
Abhishek_Rai <raigon48@gmail.com>
2020-05-17 15:24:05 +0530

    arrays and functions

---
+++ basicProgram/arrays.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() 
+{
+    int luckNums[] = {4, 8 , 15, 16, 23};
+    cout<< luckNums << endl;
+    int luckyNums1[30] = {4, 15, 16};
+    cout << luckyNums1 << endl;
+    return 0;
+
+}
---
+++ basicProgram/functions.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+void sayHi(string name) {
+    cout << "Hello "<< name <<", How are you?" << endl;
+}
+
+int main() {
+    sayHi("Abhishek Rai");
+    return 0;
+}
