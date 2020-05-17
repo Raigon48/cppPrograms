b7e886a0ca65fef417ae8804f8d53b66abf74f60
Abhishek_Rai <raigon48@gmail.com>
2020-05-17 15:33:01 +0530

    return keyword

--- basicProgram/functions.cpp
+++ basicProgram/functions.cpp
@@ -5,7 +5,13 @@ void sayHi(string name) {
     cout << "Hello "<< name <<", How are you?" << endl;
 }
 
+int cube(int num) {
+    return num*num*num;
+}
+
 int main() {
     sayHi("Abhishek Rai");
+    int resultOf3Cube = cube(3);
+    cout << "cube of 3 is : " << resultOf3Cube << endl;
     return 0;
 }
